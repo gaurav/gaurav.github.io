7674b2aa6f37350e036f1384629b6b618b7a49e9
gaurav <gaurav@ggvaidya.com>
2018-01-21 16:15:15 -0500

    Added files from the Internet Archive.

---
+++ sps.nus.edu.sg/~vaidyagi/guestbook.php
@@ -0,0 +1,120 @@
+<html>
+  	<head>  		<title>Ye Olde Guestbook</title>
+  	</head>
+  	<body>
+  	
+  	<p><center><a href="http://passport.panda.org/"><img src="wwfpassport/butterfly.gif" /><img src="wwfpassport/tiger.gif" /><img src="wwfpassport/child.gif" /></a></center>
+  
+  <center>
+  <table width=98%> <!-- ADJUST THIS! -->
+  <tr>
+  	<td><h1>Ye Olde Guestbook</h1>
+  	<td>
+  <tr>
+  	<td>
+  	<td align=right><i>Or, "Just who on earth are you, really?</i>
+  <tr>
+  	<td colspan=2>
+  
+  
+  <p>	Feel free to waste your time here. The results won't be put up on the web. Thanks!
+  
+  <table>
+  <form action="./guestbook.php?file=guestbook.txt" method=POST>
+  	<tr><td>I am
+  <td><input name="I am
+  " size=80/>
+  <tr><td>I think
+  <td><input name="I think
+  " size=80/>
+  <tr><td>I know
+  <td><input name="I know
+  " size=80/>
+  <tr><td>I want
+  <td><input name="I want
+  " size=80/>
+  <tr><td>I wish
+  <td><input name="I wish
+  " size=80/>
+  <tr><td>I miss
+  <td><input name="I miss
+  " size=80/>
+  <tr><td>I fear
+  <td><input name="I fear
+  " size=80/>
+  <tr><td>I hear
+  <td><input name="I hear
+  " size=80/>
+  <tr><td>I wonder
+  <td><input name="I wonder
+  " size=80/>
+  <tr><td>I regret
+  <td><input name="I regret
+  " size=80/>
+  <tr><td>I love
+  <td><input name="I love
+  " size=80/>
+  <tr><td>I ache
+  <td><input name="I ache
+  " size=80/>
+  <tr><td>I care
+  <td><input name="I care
+  " size=80/>
+  <tr><td>I will not
+  <td><input name="I will not
+  " size=80/>
+  <tr><td>I need
+  <td><input name="I need
+  " size=80/>
+  <tr><td>I should
+  <td><input name="I should
+  " size=80/>
+  <tr><td>One song I really like right now
+  <td><input name="One song I really like right now
+  " size=80/>
+  <tr><td>One poem I really like right now
+  <td><input name="One poem I really like right now
+  " size=80/>
+  <tr><td>One book I really like right now
+  <td><input name="One book I really like right now
+  " size=80/>
+  <tr>
+  <td align=center colspan=2>
+  	<button type=submit>Submit!</button>
+  </table>
+  </form>
+  
+  </table>
+  </center>
+  </table>
+  </center>
+  
+  <hr>
+  Back to the main page, <a href="index.php">this way</a>.
+  <!-- Insert 'See also's, etc here -->
+  	
+  
+  
+  
+  
+  <!--
+       FILE ARCHIVED ON 3:53:50 Sep 12, 2005 AND RETRIEVED FROM THE
+       INTERNET ARCHIVE ON 2:44:55 Apr 21, 2012.
+       JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+  
+       ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+       SECTION 108(a)(3)).
+  -->
+  
+  
+  
+  
+  
+  
+  
+  
+  <script type="text/javascript">
+          </script>
+  
+  </body>
+  </html>
---
+++ sps.nus.edu.sg/~vaidyagi/verse/dates.php
@@ -0,0 +1,450 @@
+<html>
+  	<head>  		<title>Dates of Songs Added</title>
+  	</head>
+  	<body>
+  	
+  	<p><center><a href="http://passport.panda.org/"><img src="../wwfpassport/butterfly.gif" /><img src="../wwfpassport/tiger.gif" /><img src="../wwfpassport/child.gif" /></a></center>
+  
+  <p><center>
+  <table width=98%> <!-- ADJUST THIS! -->
+  <tr>
+  	<td><h2>Dates of Songs Added</h2>
+  	<!-- note: old template used h1! -->
+  	<td>
+  <tr>
+  	<td colspan=2>
+  <!-- Text goes here -->
+  <p>For my own record, largely, I've got here a list of the songs on this page along with the date on which they
+  were added. If you read anything into all this, lemme know :). <A href="./">Click here</a> to return to the lyrics
+  page.
+  
+  <p><b>Months:</b> 
+  	<i>2003</i> <a href="#aug2003">August</a> <a href="#sep2003">September</a> <a href="#oct2003">October</a> <a href="#nov2003">November</a> <a href="#dec2003">December</a><br />
+  	<i>2004</i> <a href="#jan2004">January</a> <a href="#feb2004">February</a> <a href="#mar2004">March</A> April May June <a href="#jul2004">July</a> August September <a href="#oct2004">October</a> <a href="#nov2004">November</a> Decemeber<br />
+  	<i>2005</i> January <a href="#feb2005">February</a>
+  
+  <center>
+  <table Border=1>
+  	<tr>
+  		<td>No
+  		<td>Date
+  		<td>Song
+  		<td>Section
+  	<tr>
+  		<td colspan=4><center><b>2003</b></center>
+  	<tr>
+  		<td>1
+  		<a name="aug2003">
+  		<td>August 13
+  		<td><a href="perfect_day.html">Perfect Day</a>
+  		<td><a href="./#cute">Cute</a>
+  		</a>
+  	<tr>
+  		<td>2
+  		<td>August 13
+  		<td><A href="catch_the_wind_ii.html">Catch The Wind (ii)</a>
+  		<td><A href="./#love">Love</a>
+  	<tr>
+  		<td>3
+  		<td>August 22
+  		<td><a href="mamma.html">Mamma</a>
+  		<td><a href="./#personal">Personal</a>
+  	<tr>
+  		<td>4
+  		<td>August 22
+  		<td><A href="you_give_me_love.html">You Give Me Love</a>
+  		<td><A href="./#love">Love</a>
+  	<tr>
+  		<td>5
+  		<td>August 23
+  		<td><A href="loverly.html">Loverly</a>
+  		<td><a href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>6
+  		<td>August 23
+  		<td><a href="food_glorious_food.html">Food, Glorious Food</a>
+  		<td><a href="./#cute">Cute</a>
+  	<tr>
+  		<td>7
+  		<td>August 24
+  		<td><a href="lukannon.html">Lukannon</a>
+  		<td><a href="./#conservation">Conservation</a>
+  	<tr>
+  		<td>8
+  		<td>August 30
+  		<td><a href="if.html">If</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>9
+  		<td>August 30
+  		<td><a href="the_road_not_taken.html">The Road Not Taken</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>10
+  		<td>August 30
+  		<td><a href="stopping_by_the_woods_on_a_snowy_evening.html">Stopping by the Woods on a Snowy Evening</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>11
+  		<td>August 30
+  		<td><a href="hey_jude.html">Hey Jude</a>
+  		<td><a href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>12
+  		<td>August 30
+  		<td><a href="lemon_tree.html">Lemon Tree</a>
+  		<td><a href="./#personal">Personal</a>
+  	<tr>
+  		<td>13
+  		<a name="sep2003">
+  		<td>September 1
+  		<td><a href="across_the_universe.html">Across The Universe</a>
+  		<td><a href="./#interesting">Interesting</a>
+  		</a>
+  	<tr>
+  		<td>14
+  		<Td>September 6
+  		<td><a href="time_of_your_life.html">Time Of Your Life</a>
+  		<td><a href="./#love">Love</a>
+  	<tr>
+  		<td>15
+  		<td>September 6
+  		<td><a href="tequila_sunrise.html">Tequila Sunrise</a>
+  		<td><a href="./#sad">Sad</a>
+  	<tr>
+  		<td>16
+  		<Td>September 6
+  		<td><A href="rocky_mountain_high.html">Rocky Mountain High</a>
+  		<td><A href="./#conservation">Conservation</a>
+  	<tr>
+  		<td>17
+  		<Td>September 6
+  		<Td><a href="annies_song.html">Annie's Song</a>
+  		<td><A href="./#love">Love</a>
+  	<tr>
+  		<td>18
+  		<td>September 7
+  		<td><a href="hope.html">Hope</a>
+  		<td><a href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>19
+  		<td>September 7
+  		<td><a href="the_merry_inn.html">The Merry Inn</a>
+  		<td><a href="./#cute">Cute</a>
+  	
+  	<tr>
+  		<td>20
+  		<td>September 21
+  		<td><a href="tiger_tiger.html">Tiger, Tiger</a>
+  		<td><a href="./#conservation">Conservation</a>
+  	<tr>
+  		<td>21
+  		<td>September 24
+  		<td><a href="reluctance.html">Reluctance</a>
+  		<td><a href="./#sad">Sad</a>
+  	<tr>
+  		<td>22
+  		<td>September 24
+  		<td><a href="una_paloma_blanca.html">Una Paloma Blanca</a>
+  		<td><a href="./#freedom">Freedom</a>
+  	<tr>
+  		<td>23
+  		<td>September 25
+  		<td><a href="human_family.html">Human Family</a>
+  		<td><a href="./#interesting">Interesting</a>
+  	<tr>
+  		<td>24
+  		<a name="oct2003">
+  		<td>October 8
+  		<td><a href="beauty_and_the_beast.html">Beauty and the Beast</a>
+  		<td><A href="./#love">Love</a>
+  		</a>
+  	<tr>
+  		<td>25
+  		<td>October 8
+  		<td><a href="scrambled.html">Scrambled</a>
+  		<td><a href="./#cute">Cute</a>
+  	<tr>
+  		<td>26
+  		<td>October 8
+  		<td><a href="let_it_be.html">Let It Be</a>
+  		<td><A href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>27
+  		<td>October 8
+  		<td><a href="my_favourite_things.html">My Favourite Things</a>
+  		<td><a href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>28
+  		<td>October 8
+  		<td><a href="getting_better.html">Getting Better</a>
+  		<td><a href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>29
+  		<td>October 12
+  		<td><A href="bare_necessities.html">Bare Necessities</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>30
+  		<td>October 27
+  		<td><a href="the_long_and_winding_road.html">The Long and Winding Road</a>
+  		<td><a href="./#others">Others</a>
+  	<tr>
+  		<td>31
+  		<td>October 27
+  		<td><a href="strawberry_fields_forever.html">Strawberry Fields Forever</a>
+  		<td><A href="./#SFF">SFF</a>
+  	<tr>
+  		<td>32
+  		<td>October 27
+  		<td><a href="jealous_guy.html">Jealous Guy</a>
+  		<td><a href="./#SFF">SFF</a>
+  	<tr>
+  		<td>33
+  		<td>October 27
+  		<td><a href="sk8tr_boi.html">Sk8tr Boi</a>
+  		<td><a href="./#SFF">SFF</a>
+  	<tr>
+  		<td>34
+  		<td>October 27
+  		<td><a href="walk_with_me.html">Walk With Me</a>
+  		<td><a href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>35
+  		<Td>October 27
+  		<td><a href="im_a_loser.html">I'm A Loser</a>
+  		<td><a href="./#SFF">SFF</a>
+  	<tr>
+  		<td>36
+  		<td>October 27
+  		<td><a href="youve_got_to_hide_your_love_away.html">You've Got To Hide Your Love Away</a>
+  		<td><a href="./#SFF">SFF</a>
+  	<tr>
+  		<td>37
+  		<td>October 28
+  		<td><a href="white_flag.html">White Flag</a>
+  		<td><a href="./#love">Love</a>
+  	<tr>
+  		<td>38
+  		<a name="nov2003">
+  		<td>November 1
+  		<td><A href="where_the_mind_is_without_fear.html">Where The Mind Is Without Fear</a>
+  		<td><A href="./#life">Life</a>
+  		</a>
+  	<tr>
+  		<td>39
+  		<td>November 3
+  		<td><a href="waltzing_matilda.html">Waltzing Matilda</a>
+  		<td><a href="./#fun">Fun</a>
+  	<tr>
+  		<td>40
+  		<td>November 13
+  		<td><a href="make_me_a_channel.html">Make Me A Channel Of Your Peace</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>41
+  		<td>November 13
+  		<td><a href="when_im_sixty_four.html">When I'm Sixty-Four</a>
+  		<td><a href="./#love">Love</a>
+  	<tr>
+  		<td><b>42</b>
+  		<td>November 13
+  		<td><a href="in_my_life.html">In My Life</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>43
+  		<td>November 13
+  		<td><a href="this_boy.html">This Boy</a>
+  		<td><a href="./#interesting">Interesting</a>
+  	<tr>
+  		<td>44
+  		<Td>November 21
+  		<td><a href="sea_fever.html">Sea Fever</a>
+  		<td><a href="./#adventure">Adventure</a>
+  	<tr>
+  		<td>45
+  		<td>November 27
+  		<td><a href="the_final_frontier.html">The Final Frontier (Mad About You)</a>
+  		<td><a href="./#love">Love</a>
+  	<tr>
+  		<td>46
+  		<td>November 27
+  		<td><a href="the_actor.html">The Actor</a>
+  		<td><a href="./#sad">Sad</a>
+  	<tr>
+  		<td>47
+  		<a name="dec2003">
+  		<td>December 1
+  		<td><a href="mother.html">Mother</a>
+  		<td><a href="./#sad">Sad</a>
+  		</a>
+  	<tr>
+  		<td>48
+  		<Td>December 1
+  		<td><a href="crippled_inside.html">Crippled Inside</a>
+  		<td><a href="./#fun">Fun</a>
+  	<tr>
+  		<td>49
+  		<td>December 5
+  		<td><a href="pretty_fly_for_a_rabbi.html">Pretty Fly (For A Rabbi)</a>
+  		<td><a href="./#fun">Fun</a>
+  	<tr>
+  		<td>50
+  		<td>December 5
+  		<td><a href="every_breath_you_take.html">Every Breath You Take</a>
+  		<td><a href="./#love">Love</a>
+  	<tr>
+  		<td>51
+  		<td>December 11
+  		<td><a href="memories.html">Memories</a>
+  		<td><a href="./#sad">Sad</a>
+  	<tr>
+  		<td>52
+  		<Td>December 11
+  		<td><A href="free_as_a_bird.html">Free As A Bird</a>
+  		<td><a href="./#love">Love</a>
+  	<tr>
+  		<td>53
+  		<Td>December 11
+  		<td><a href="i_hope_you_dance.html">I Hope You Dance</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>54
+  		<td>December 13
+  		<td><A href="a_man_doesnt_have_time_in_his_life.html">A Man Doesn't Have Time In His Life</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>55
+  		<td>December 31 (at 3:42pm!)
+  		<td><a href="for_no_one.html">For No One</a>
+  		<td><a href="./#sad">Sad</a>
+  	<tr>
+  		<td colspan=4><Center><b>2004</b></center>
+  	<tr>
+  		<td>56
+  		<a name="jan2004">
+  		<td>January 7
+  		<td><A href="thomas_o_malley_cat.html">Thomas O'Malley Cat</a>
+  		<td><a href="./#fun">Fun</a>
+  		</a>
+  
+  	<tr>
+  		<td>57
+  		<td>January 19
+  		<td><A href="auld_lang_syne.html">Auld Lang Syne</a>
+  		<td><A href="./#interesting">Interesting</a>
+  	<tr>
+  		<td>58
+  		<td>January 25
+  		<td><A href="beautiful_boy.html">Beautiful Boy</a>
+  		<td><A href="./#love">Love</a>
+  	<tr>
+  		<td>59
+  		<a name="feb2004">
+  		<td>February 16
+  		<td><A href="something_to_talk_about.html">Something To Talk About</a>
+  		<td><A href="./#others">Others</a>
+  	<tr>
+  		<td>60
+  		<td>February 16
+  		<td><A href="searchin_my_soul.html">Searchin' My Soul</a>
+  		<td><a Href="./#cheerup">Cheer Up</a>
+  	<tr>
+  		<td>61
+  		<td>February 19
+  		<td><a href="every_town_a_home_town.html">Every Town A Home Town</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>62
+  		<a name="mar2004">
+  		<td>March 5
+  		<td><A href="all_star.html">All Star</a>
+  		<td><a href="./#fun">Fun</a>
+  	<tr>
+  		<td>63
+  		<td>March 27
+  		<td><a href="death_be_not_proud.html">Death, Be Not Proud</a>
+  		<td><a href="./#life">Life</a>
+  	<tr>
+  		<td>64
+  		<a name="jul2004" />
+  		<td>July 9
+  		<td><A href="mamma_mia.html">Mamma Mia</a>
+  		<td><A href="./#love">Love</a>
+  	<tr>
+  		<td>65
+  		<td>July 9
+  		<td><A href="bop_bop_baby.html">Bop Bop Baby</a>
+  		<td><A href="./#goodbye">Goodbye</a>
+  	<tr>
+  		<td>66
+  		<td>July 9
+  		<td><A href="queen_of_my_heart.html">Queen of my Heart</a>
+  		<td><A href="./#goodbye">Goodbye</a>
+  	<tr>
+  		<td>67
+  		<td>July 31
+  		<td><a href="./kiss_me.html">Kiss Me</a>
+  		<td><A href="./#romantic">Romantic</a>
+  	<tr>
+  			<td>68
+  		<A Name="oct2004">
+  			<td>October 15
+  			<td><a href="./smile.html">Smile</a>
+  			<td><A href="./#romantic">Romantic</a>
+  		</a>
+  	<tr>
+  			<td>69
+  		<a name="nov2004">
+  			<td>November 19
+  			<td><a href="./november_rain.html">November Rain</a>
+  			<td><a href="./#love">Love</a>
+  		</a>
+  	<tr>
+  		<td colspan="4">
+  			<center><b>2005</b></center>
+  		</td>
+  	<tr>
+  			<td>70
+  		<a name="feb2005">
+  			<td>February 25
+  			<td><a href="./mockingbird.html">Mockingbird</a>
+  			<td><a href="./#life">Life</a>
+  </table>
+  </center>
+  
+  <!-- Text ends here -->
+  <p>
+  <tr>
+  </table>
+  </center>
+  
+  <hr>
+  Back to the main page, <a href="../index.php">this way</a>.
+  <!-- Insert 'See also's, etc here -->
+  	
+  
+  
+  
+  
+  <!--
+       FILE ARCHIVED ON 12:36:15 May 22, 2008 AND RETRIEVED FROM THE
+       INTERNET ARCHIVE ON 3:04:44 Apr 21, 2012.
+       JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+  
+       ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+       SECTION 108(a)(3)).
+  -->
+  
+  
+  
+  
+  
+  
+  
+  
+  <script type="text/javascript">
+          </script>
+  
+  </body>
+  </html>
