76ae44c307befcb30567a9472d9499e232f2e500
Gaurav Vaidya <gaurav@renci.org>
2020-08-25 15:39:58 -0400

    Updated format of websites.json.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
@@ -7,43 +7,38 @@ import SEO from "../components/seo"
 
 import indexStyles from "./index.module.css"
 
-export const query = graphql`
-  query {
-    dataJson(_id: { eq: "https://www.ggvaidya.com/" }) {
-      sameAs {
-        _id
-        label
-        publisher {
-          logo
-          name
+export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      dataJson(_id: {eq: "https://www.ggvaidya.com/"}) {
+        sameAs {
+          _id
+          label
+          publisher {
+            logo
+            name
+          }
         }
       }
     }
-  }
-`
+  `)
 
-export default function IndexPage({ data }) {
   return (
     <Layout>
       <SEO title="Home" />
 
-      <p>
-        Welcome to my website! For now, there's nothing here. Instead, you can
-        find me on other websites:
-      </p>
+      <p>Welcome to my website! For now, there's nothing here. Instead,
+      you can find me on other websites:</p>
 
       <ul class={indexStyles.sameAsList}>
-        {data.dataJson.sameAs.map(({ _id, label, publisher }) => {
-          return (
-            <li>
-              <a href={_id}>{label}</a> on {publisher.name}
-            </li>
-          )
-        })}
-        <li>
-          <em>Or</em> you can e-mail me at <tt>gaurav</tt>(at)<tt>ggvaidya</tt>.
-          <tt>com</tt>.
-        </li>
+        {
+          data.dataJson.sameAs.map(({ _id, label, publisher }, index) => {
+            return (
+              <li><a href={ _id }>{ label }</a> on { publisher.name }</li>
+            );
+          })
+        }
+        <li><em>Or</em> you can e-mail me at <tt>gaurav</tt>(at)<tt>ggvaidya</tt>.<tt>com</tt>.</li>
       </ul>
     </Layout>
   )
